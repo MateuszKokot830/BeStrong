1fbb51b77b7d37342424dcd375f4460f9c9a6e9d
MateuszKokot830 <MateuszKokot830@wp.pl>
2022-10-30 12:26:13 +0100

    Added: navbar component + angular folder structure

---
+++ WebAPI/Client/src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from 'src/app/core/services/account.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  model: any = {}
+  loggedIn: boolean;
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  login()
+  {
+    this.accountService.login(this.model).subscribe(respone => {
+      console.log(respone);
+      this.loggedIn = true;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+}
