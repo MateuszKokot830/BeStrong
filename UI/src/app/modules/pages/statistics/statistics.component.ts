376d72cf12c358100b77254db92a3dcff6f24046
MateuszKokot830 <MateuszKokot830@wp.pl>
2023-01-04 10:40:23 +0100

    Added statistics page design

--- UI/src/app/modules/pages/statistics/statistics.component.ts
+++ UI/src/app/modules/pages/statistics/statistics.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs/operators';
+import { Exercise } from 'src/app/core/models/Exercise';
+import { UserAuth } from 'src/app/core/models/User';
+import { AccountService } from 'src/app/core/services/account.service';
+import { UserService } from 'src/app/core/services/user.service';
+import { WorkoutService } from 'src/app/core/services/workout.service';
 
 @Component({
   selector: 'app-statistics',
@@ -6,8 +13,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistics.component.css']
 })
 export class StatisticsComponent implements OnInit {
+  currentUser: UserAuth | null = null;
+  exercises: Exercise[] = [];
+  calculatorLift: number;
+  calculatorReps: number;
 
-  constructor() { }
+  constructor(private workoutService: WorkoutService, private accountService: AccountService,
+    private userService: UserService, private toastr: ToastrService) {
+      this.accountService.currentUser$.pipe(take(1)).subscribe({
+        next: currentUser => this.currentUser = currentUser
+      });
+    }
 
   ngOnInit(): void {
   }
